a2469ae40dd2139ed224643c8eafecbef31cddf8
horaciov <horaciov@gmail.com>
2022-04-01 15:53:53 -0400

    test simulation

--- nsga3_simulated.py
+++ nsga3_simulated.py
@@ -1,10 +1,12 @@
+from matplotlib import markers
 from numpy import random,mean,std,min,max
 from math import factorial
 from deap import base, creator, tools, algorithms
 from data import C,D
 from objetivefunctions import f1,f2,f3
 from constraint import validateConstraints
-
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation 
 
 #Parameters
 CLASS_SIZE=len(C)
@@ -16,6 +18,8 @@ MU = int(H + (4 - H % 4)) #Population size
 NGEN = 40 # Number of generations
 CXPB = 1.0 #crossover probability
 MUTPB = 1.0 #chance to mutate
+
+pop = []
 
 ref_points = tools.uniform_reference_points(NOBJ, P)
 
@@ -39,17 +43,47 @@ def evaluate(X):
 
 toolbox.register("evaluate", evaluate)
 
-#Generate and evaluate the entire population
-pop = []
-while(len(pop)<MU):
-    ind=toolbox.individual()
-    if validateConstraints(ind):
-        ind.fitness.values=evaluate(ind)
-        pop.append(ind)
+
+# Initialize statistics object
+stats = tools.Statistics(lambda ind: ind.fitness.values)
+stats.register("avg", mean, axis=0)
+stats.register("std", std, axis=0)
+stats.register("min", min, axis=0)
+stats.register("max", max, axis=0)
+
+logbook = tools.Logbook()
+logbook.header = "gen", "evals", "std", "min", "avg", "max"
+
+
+fig = plt.figure()
+ax = fig.add_subplot(projection='3d')
+ax.set_xlabel('X Average Distance')
+ax.set_ylabel('Y Average Estableshment')
+ax.set_zlabel('Z Average Course')
+
+def init():
+    global fig
+    print(0)
+    #Generate and evaluate the entire population
+ 
+    while(len(pop)<MU):
+        ind=toolbox.individual()
+        if validateConstraints(ind):
+            ind.fitness.values=evaluate(ind)
+            pop.append(ind)
+
+    for i1 in range(len(pop)):
+        xs=pop[i1].fitness.values[0]
+        ys=pop[i1].fitness.values[1]
+        zs=pop[i1].fitness.values[2]
+        ax.scatter(xs,ys,zs)    
+
 
 
 # Begin the generational process
-for gen in range(1, NGEN):
+def animate(i):
+    print(i)
+    global pop, ax
     offspring = algorithms.varAnd(pop, toolbox, CXPB, MUTPB)
     
     #Discard invalid individual
@@ -64,6 +98,45 @@ for gen in range(1, NGEN):
     # Select the next generation population from parents and offspring
     pop = toolbox.select(pop + offspring, MU)
 
+     
+
+
+
+#anim = FuncAnimation(fig, animate, init_func = init,
+ #                   frames = NGEN, interval = 20)
+  
+   
+#anim.save('continuousSineWave.mp4', writer = 'ffmpeg', fps = 2)
+
+init()
+animate(0)
+animate(0)
+animate(0)
+animate(0)
+animate(0)
+animate(0)
+animate(0)
+animate(0)
+animate(0)
+animate(0)
+animate(0)
+animate(0)
+animate(0)
+
+
+plt.clf()
+ax = fig.add_subplot(projection='3d')
+ax.set_xlabel('X Average Distance')
+ax.set_ylabel('Y Average Estableshment')
+ax.set_zlabel('Z Average Course')
+    
+for i1 in range(len(pop)):
+    xs = pop[i1].fitness.values[0]
+    ys = pop[i1].fitness.values[1]
+    zs = pop[i1].fitness.values[2]   
+    ax.scatter(xs,ys,zs)   
+
+plt.show()
 
 #Non-dominated solutions
 ND=[]
@@ -82,6 +155,3 @@ print("Población: "+str(len(pop)))
 print("Cantidad de soluciones no dominadas: "+str(len(ND)))
 for i in ND:
     print(str(i) + " "+str(i.fitness.values))
-
-
-
