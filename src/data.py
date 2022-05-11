bbd376eee67e5a5e076a06241b2de4fb36c70904
horaciov <horaciov@gmail.com>
2022-05-11 10:11:14 -0400

    Remove print

--- src/data.py
+++ src/data.py
@@ -11,21 +11,18 @@ def init(maxDistance):
     
     C = []
     cur.execute("select nro,g,t,s,i,e from c order by 1")
-    print("Load C...")
     for row in cur:
         C.append([row[1],row[2],row[3],row[4],row[5]])
    
     #Teachers
     D=[]
     cur.execute("select nro,lat,long from d order by 1")
-    print("Load D...")
     for row in cur:
         D.append([row[0],row[1],row[2]])
 
     #Establishment
     E=[]
     cur.execute("select nro,lat,long from e order by 1")
-    print("Load E...")
     for row in cur:
         E.append([row[0],row[1],row[2]])
 
